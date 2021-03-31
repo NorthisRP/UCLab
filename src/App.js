144435bb67b07dcf496173ec5baf7313c193ab06
NorthisRP <philw7321980@gmail.com>
2021-04-01 01:54:14 +0300

    Header component with Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import Header from "./Components/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./Pages/About";
+import Landing from "./Pages/Landing";
+import Blog from "./Pages/Blog";
+import Projects from "./Pages/Projects";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/blog" component={Blog} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
